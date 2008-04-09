b3a58c2e0bea030a1b43ed240d14f691f631a29f
WhiteAls <Als@admin.ru.net>
2008-04-09 21:21:48 +0000

    version bump. yes, i once again forgot to do this :)
    git-svn-id: file:///mnt/d/python/talisman-svn/svn/dump/tags/1@73 95dd5e0d-2831-4fbb-8c2f-55e3b97bb653

--- pybot.py
+++ pybot.py
@@ -760,8 +760,7 @@ def iqHnd(con, iq):
 		result = iq.buildReply('result')
 		query = result.getTag('query')
 		query.setTagData('name', 'ταλιςμαη')
-#		query.setTagData('version', 'ver.1 (svn rev 72) [antiflood]')
-		query.setTagData('version', 'ver.1 (author ver) [antiflood]')
+		query.setTagData('version', 'ver.1 (svn rev 73) [antiflood]')
 		query.setTagData('os', osver)
 		JCON.send(result)
 		raise xmpp.NodeProcessed
